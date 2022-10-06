65417399ea92b81ebfcf7ec0d347468f4d911693
Magdalena <magdalenadrejowicz@gmail.com>
2022-10-06 10:34:14 +0200

    homework week 01

---
+++ pages/users.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const users = () => {
+  return <div>Tresc bez folderu users bez folderu</div>;
+};
+
+export default users;
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Tresc z pliku index z folderu users/index.jsx</div>;
+};
+
+export default index;
