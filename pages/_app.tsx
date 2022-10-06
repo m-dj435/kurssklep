c6be80e1cb9c96e09a0b12dd142e2815704c3074
Magdalena <magdalenadrejowicz@gmail.com>
2022-10-06 10:38:39 +0200

    Lekcja 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <p className="text-5xl font-bold text-center text-red-500 mt-6 border-8 rounded-3xl border-green-300 shadow-lg shadow-pink-400">
+        Hej
+      </p>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
