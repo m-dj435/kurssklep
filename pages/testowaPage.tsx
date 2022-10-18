6b92f2e4e2990f18659ddf07926cc43b11ea1fb6
Magdalena <magdalenadrejowicz@gmail.com>
2022-10-18 14:09:05 +0200

    blad do spra

--- pages/testowaPage.tsx
+++ pages/testowaPage.tsx
@@ -1,18 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 
-function Example() {
-  // Declare a new state variable, which we'll call "count"
+function MyButton({ count, onClick }) {
+  return (
+    <>
+      <button onClick={onClick} className="border-2 bg-red-300">
+        you clicked {count} times
+      </button>
+    </>
+  );
+}
+
+const user = {
+  name: "Ramen",
+  imageUrl: "https://i.imgur.com/HT0UPeN.jpeg",
+};
+
+function MyApp() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>Yo! This is my APp</h1>
+      <p>{user.name}</p>
+      <Image
+        className="awatar"
+        src={user.imageUrl}
+        alt={"photo of best creature"}
+        layout="fixed"
+        width={800}
+        height={500}
+      />
+      <br />
+      <MyButton count={count} onClick={handleClick} />
+      <br />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
 
-export default Example;
+export default MyApp;
