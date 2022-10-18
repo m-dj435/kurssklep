7d46109f0ee8da762019d0ae0add733f0087c6b0
Magdalena <magdalenadrejowicz@gmail.com>
2022-10-18 11:36:16 +0200

    nauka useState

---
+++ pages/testowaPage.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(null);
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default Example;
